302f94a08ced4df0b458145d12507aee4210d045
Jan Koczuba <schizobi@gmail.com>
2025-02-20 07:01:50 +0100

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Jan Koczuba on 20/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CryptoTrackerTests/CryptoTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerTests.swift
+//  CryptoTrackerTests
+//
+//  Created by Jan Koczuba on 20/02/2025.
+//
+
+import Testing
+@testable import CryptoTracker
+
+struct CryptoTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
